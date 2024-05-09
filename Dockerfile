b1d2a339e66564a1af6971000e4ac2c10e96bb53
Alex Lin <alex.stepit@gmail.com>
2024-05-09 20:48:37 +0300

    Add project Vlad

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
-Test
+# Використовуємо офіційний образ Python версії 3.9
+FROM python:3.9
+
+# Встановлюємо Flask та бібліотеку requests
+RUN pip install Flask requests
+
+# Створюємо робочий каталог /app та копіюємо усі файли проекту туди
+WORKDIR /app
+COPY . /app
+
+# Вказуємо, що контейнер буде слухати порт 5000
+EXPOSE 5000
+
+# Команда для запуску сервера Flask
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-asdas
-asdasd
-as
-d
-asd
-as
-das
-das
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = requests.get('https://api.thecatapi.com/v1/images/search')
+    data = response.json()
+    image_url = data[0]['url']
+    return render_template('index.html', image_url=image_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
